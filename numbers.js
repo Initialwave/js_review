32ea7f8eb37bf1886167028d2404e813caa9a484
DANIEL <asylumgames39@gmail.com>
2021-08-21 15:10:07 -0500

    added additional files

---
+++ numbers.js
@@ -0,0 +1,24 @@
+console.log("numbers.js")
+
+// Create a variable using the let keyword and assign a number to it.
+let amount = 15;
+console.log(amount);
+
+// On a new line, add 15 to your variable
+amount = amount + 15;
+console.log(amount);
+
+// On a new line, subtract 8 from your variable without using the variable when calculating the new value
+amount = amount - 8;
+console.log(amount);
+
+// Calculate the average of 26, 44, 72, 86, 112. Assign the calculated average to a new variable.
+let result;
+result = (26 + 44 + 72 + 86 + 112) / 5;
+console.log(result);
+// Modify the following code so it outputs 15 without adding any new lines of code.
+let theNumber = 14;
+console.log(theNumber+1)
+
+// What operator can you use to find out if a variable contains a number?
+// typeof number
